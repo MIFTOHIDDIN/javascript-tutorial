370b9a9ae79c7d153c5e616a92ff71d06b084e6c
MIFTOHIDDIN <mmamasobirov@mail.ru>
2023-03-16 23:45:52 +0500

    6-dars

---
+++ js 6-dars/main.js
@@ -0,0 +1,60 @@
+
+// obecjtlar
+
+// let name = "webbrain"
+// let year = 2019
+// let faculty = "frontend"
+// let name = "title"
+// let obj = { name: name, year: 2019, "full name": "it center", name: "eshmat" }
+
+// let title = "year"
+
+// obj.age = 12
+// delete obj.year
+
+// console.log(obj["full name"] );
+// console.log(obj[title]);
+
+// let name = "title"
+// let obj = { name, year: 2019, "full name": "it center" }
+// console.log(obj);
+
+// let name = "title"
+// let obj = { name, year: 2019, "full name": "it center" }
+
+// console.log("names" in obj);
+
+// for(let i in obj){
+//     console.log(obj[i]);
+// }
+
+// let obj1 = { name: "webbrain", year: 2019, "full name": "it center" }
+// let obj2 = {
+//     name: "pdp",
+//     year: 2020
+// }
+// obj1 = 12
+// Object.assign(obj1,obj2)
+// console.log(obj1);
+// console.log(obj1 === obj2);
+
+// garbage collection 
+
+// this
+
+// let obj1 = {
+//     name: "webbrain",
+//     year: 2019,
+//     info:{name:"academy"}
+ 
+// }
+
+// console.log({...obj1});
+
+// new.target
+
+function getData(){
+console.log(new.target);
+}
+
+new getData()
