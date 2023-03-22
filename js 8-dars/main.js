5e1b4f10a58417e617c040957bedeb83f46cd08f
MIFTOHIDDIN <mmamasobirov@mail.ru>
2023-03-23 00:42:15 +0500

    8-dars

---
+++ js 8-dars/main.js
@@ -0,0 +1,52 @@
+// String
+// let str1 = 'Webbrain "IT" center'
+// let str2 = "Webbrain"
+// let str3 = `Webbrain ${str1}`
+
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+// let str = "Abs"
+
+
+
+// console.log(str[str.length - 2]);
+// console.log(str.charAt(0));
+// console.log(str.at(-1));
+// console.log(str.toLowerCase());
+// console.log(str.toUpperCase());
+// console.log(str.indexOf("brain",5));
+// console.log(str.lastIndexOf("brain",5));
+// console.log(str.includes("brains"));
+// console.log(str.startsWith("+998"));
+// console.log(str.endsWith("07441"));
+// console.log(str.padStart(str.length + 1, "+"));
+// console.log(str.slice(2,5));
+// console.log(str.substring(2,5));
+// console.log(str.substr(2,5));
+// console.log(eval("2+4*3"));
+
+// console.log(str.trim());
+// console.log(str.split(","));
+// console.log(str.indexOf("brain"));
+// console.log(str.search(/brain/i));
+// console.log(str.localeCompare("abs"));
+
+// =========
+
+// Array
+// let ar = [1,2,3,4]
+// ar.unshift(0)
+// console.log(ar);
+// let ar = [0,1,2,3,4]
+// let ar1 =[5,6,7,8]
+
+// console.log(ar.slice(3,5));
+// console.log(ar.splice(3,1,"uch"));
+// console.log(ar);
+// console.log(ar.concat(ar1));
+// console.log(ar.indexOf(1));
+// console.log(ar.includes(2));
+// console.log(ar.lastIndexOf(2));
+// console.log(ar.reverse());
+// console.log(ar.join("*"));
