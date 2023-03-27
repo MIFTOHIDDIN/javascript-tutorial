5b8ad8946c68ca0709fb030d79db151effa253b7
MIFTOHIDDIN <mmamasobirov@mail.ru>
2023-03-27 21:47:10 +0500

    11-dars

---
+++ js 11-dars/main.js
@@ -0,0 +1,70 @@
+// dictructure  -  {},[]
+
+// let obj = { name: "eshmat", title: "toshmat", info: { age: 12 } }
+// const { name: newName, info: { age } } = obj
+// console.log(age);
+
+// function get({info:{age}}){
+// console.log(age);
+// }
+// get(obj)
+
+// let ar = ["apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana"]
+
+// const [bir,...rest] = ar
+// console.log(rest);
+// function get([bir]) {
+//     console.log(bir);
+// }
+// get(["orange", "apple"])
+// ============
+// new Date()
+// let time = new Date()
+// console.log(`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`);
+// =========
+// new Map(),new Set()
+// let map = new Map()
+// let obj = {}
+// map.set("title","webbrain") 
+// map.set("name","IT center")
+// map.get()
+// map.delete()
+// map.clear()
+
+// console.log(map);
+// let set = new Set()
+// let ar = []
+// set.add("title")
+// set.add("webbrain")
+// set.add("webbrain")
+// set.add("webbrain")
+// set.add("webbrain")
+// set.add("webbrain")
+// set.delete("webbrain")
+// set.clear()
+
+// console.log(set);
+
+// new WeakMap(),new WeakSet()
+// let map = new WeakMap()
+// let set = new WeakSet()
+
+// ========
+
+// ==============
+// let ar = [1, 3, 2, 8, 5, 2, 3, 5]
+
+// for (let i = 0; i < 5; i++) {
+    
+//     ar.sort((a, b) => b - a)
+   
+//     let sum = ar[0] - ar[1]
+    
+//     ar.splice(0, 2)
+//     if (sum !== 0) {
+//         ar.push(sum)
+//     }
+
+// }
+// console.log(ar);
+
