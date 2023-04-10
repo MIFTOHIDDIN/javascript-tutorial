67a40801fae87c538ffc97cce91b9c2f488b1c90
MIFTOHIDDIN <mmamasobirov@mail.ru>
2023-04-10 15:23:18 +0500

    DOM,setAttribute,offsetWidth

---
+++ js 18-dars/main.js
@@ -0,0 +1,53 @@
+// DOM
+// node,element
+// console.log(document.body.childNodes);
+// console.log(document.body.children);
+// console.log(document.body.firstChild.nextSibling);
+// console.log(document.body.firstElementChild.nextElementSibling);
+
+// console.log(document.getElementById("test"));
+// console.log(document.getElementsByClassName("module"));
+// console.log(document.getElementsByTagName("h1"));
+// console.log(document.getElementsByName("res"));
+
+// console.log(document.querySelector("h1"));
+// console.log(document.querySelectorAll("h1"));
+
+// let test = document.getElementById("test")
+// let module = document.querySelector(".module")
+
+// console.log(test.innerHTML = "<h1 >Module</h1>");
+// console.log(test.outerHTML);
+// console.log(module.textContent = "<h1 >Module</h1>");
+
+// test.hidden = true
+
+// setAttribute
+
+// let button = document.querySelector("#button")
+// const change = ()=>{
+    // // module.setAttribute("class","red")
+    // // console.log(module.getAttribute("class"));
+    // // console.log(module.hasAttribute("class"));
+    // // button.removeAttribute("disabled")
+    // // module.classList.add("red")
+    // // module.classList.remove("red")
+    // module.classList.toggle("red")
+    // }
+
+
+
+    // let module = document.querySelector(".module")
+     
+    // module.style.backgroundColor = "red"
+ // module.style.cssText = "font-size:18px;color:blue;"
+
+//  let box = document.querySelector(".box")
+
+//  console.log(box.offsetWidth);
+//  console.log(box.clientWidth);
+//  console.log(box.clientHeight);
+//  console.log(box.clientLeft);
+//  console.log(box.clientTop); 
+//  console.log(box.offsetLeft); 
+//  console.log(box.offsetTop); 
